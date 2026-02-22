19785e5272d7bc62a6f40fa6cd5b77645a30c4f6
Yoza Wiratama <y_wiratama@yahoo.com>
2026-02-22 17:24:19 +0700

    Configure Vercel adapter to use Node.js 20.x runtime

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
+	}
+};
 
 export default config;
